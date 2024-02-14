22966fd439c939232524aa90acedb426958ed269
kcureton23 <kcureton23@students.desu.edu>
2024-02-13 19:00:41 -0500

    FINISH

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -9,5 +9,40 @@
  */
 public class Grok
 {
-    // implement this class per instructions in Required Activities.
+    private static final int DEFAULT_POWER_LEVEL = 50;
+    private int powerLevel;
+
+
+    public Grok() {
+        powerLevel=50;
+
+    }
+    public Grok (int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+
+    public int getPowerLevel(){
+        return powerLevel;
+    }
+
+    public void setPowerLevel(int powerLevel){
+        this.powerLevel = powerLevel;
+    }
+
+    public void takePowerPill(PowerPill pill){
+        this.powerLevel =  this.powerLevel + pill.getPower();
+    }
+
+    public void tookHit(){
+        powerLevel -= 5;
+    }
+
+     /*
+  * Adds the power level of this Grok to the power level of
+  * the pill.
+  * @param pill The PowerPill that the this Grok.  The power
+  * of the pill is added to the power level of this Grok.
+  */
+
+// implement this class per instructions in Required Activities.
 }
